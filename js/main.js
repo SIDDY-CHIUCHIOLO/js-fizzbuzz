0fea5534187bf8b2ecc051e6ec339c302a02b442
siddhartha chiuchiolo <siddychiuchiolo7@gmail.com>
2022-02-24 18:46:28 +0100

    inserito classi nel js e propietà css

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-
-let unDiv = document.querySelector("div");
+let unDiv = document.querySelector("#my-box");
 console.log("INIZIO")
 
 for (let i = 1; i < 101; i++){
     let nuovoDiv = document.createElement("div");
-    nuovoDiv.classList.add("bg-primary", "p-3");
+    nuovoDiv.classList.add("bg-primary", "m-3", "my-card", "text-center", "text-white", "fw-bold");
 
     if((i % 3 == 0) && (i % 5 == 0)) {
         nuovoDiv.innerHTML = "FizzBuzz";
